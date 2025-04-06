4931eaf96ddde8201d3ec3069a921715262ca360
ManthanKathiriya808 <manthankathiriya808@gmail.com>
2025-04-06 13:48:55 +0530

    dzsfg

---
+++ Context-video/src/contexts/TodoContext.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { useContext } from "react";
+import { createContext } from "react";
+
+
+export const TodoContext = createContext({
+    todos:[
+        {
+            id:1,
+            todo:"Todo msg",
+            completed:false
+        }
+
+    ],
+    addTodo: (todo)=> {},
+    updatedTodo: (id,todo)=>{},
+    deleteTodo:(id)=>{},
+    toggleComplete:(id)=>{}
+
+})
+
+export const useTodo = ()=>{
+    return useContext(TodoContext)
+}
+
+export const Todoprovider = TodoContext.Provider
